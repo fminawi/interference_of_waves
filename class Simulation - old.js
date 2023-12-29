a6483a847d36a604f65929192e48739ef2c7b62f
Farid <fminawi@gmail.com>
2023-12-29 20:23:26 +0200

    new

---
+++ class Simulation - old.js
@@ -0,0 +1,193 @@
+class Simulation {
+  constructor(_root, _stage, _lib){
+    this.root = _root;
+    this.stage = _stage;
+    this.lib = _lib;
+    this.container = this.root.container;
+    this.waveSpace = this.container.waveSpace;
+    this.header = this.root.header;
+    this.pz_logo = this.root.pz_logo;
+    this.playstop = this.root.playstop;
+    this.playBtn = this.playstop.playBtn;
+    this.stopBtn = this.playstop.stopBtn;
+    this.radBtn1s = this.root.radBtn1s;
+    this.radBtn2s = this.root.radBtn2s;
+    this.sliderAnimSpeed = this.root.sliderAnimSpeed;
+    this.sliderDistance = this.root.sliderDistance;
+    this.sliderFreq = this.root.sliderFreq;
+    this.sliderAmp = this.root.sliderAmp;
+    this.init();
+  }
+
+  init(){
+    var stage = this.stage,
+        lib = this.lib,
+        container = this.container,
+        waveSpace = this.waveSpace,
+        playstop = this.playstop,
+        playBtn = this.playBtn,
+        stopBtn = this.stopBtn,
+        waveSpace = this.container,
+        radBtn1s = this.radBtn1s,
+        radBtn2s = this.radBtn2s,
+        sliderAnimSpeed = this.sliderAnimSpeed,
+        sliderDistance = this.sliderDistance,
+        sliderFreq = this.sliderFreq,
+        sliderAmp = this.sliderAmp;
+    
+    const centerX = 0;
+    const centerY = waveSpace.nominalBounds.height / 2;
+    
+    var distance = 225,
+        speed = 440,
+        frequency = 8,
+        amplitude = 30,
+        wavelength = 4.4,
+        animationSpeed = 0.15,
+        res = 10,
+        playFlag = false,
+        twoSourcesFlag = true,
+        t = 0,
+        t0 = 0,
+        dt = 0,
+        width = waveSpace.nominalBounds.width - 10,
+        height = waveSpace.nominalBounds.height - 10;
+
+    var s1 = new lib.Source();
+    var s2 = new lib.Source();
+    s1.x = s2.x = centerX + 15;
+    s1.y = centerY - distance / 2;
+    s2.y = centerY + distance / 2;
+    if (twoSourcesFlag){
+      container.addChild(s1, s2);
+    } else {
+      container.addChild(s1);
+    }
+
+    function draw() {
+      if (playFlag){
+        waveSpace.removeAllChildren();
+        const g = new createjs.Graphics();  
+        const propagation = speed * t * animationSpeed;
+        for (let x = centerX; x < width; x += res) {
+          for (let y = 0; y < height; y += res) {
+            t = createjs.Ticker.getTime() / 1000 - dt;
+            const distance1 = Math.sqrt(Math.pow(y - (centerY - distance / 2), 2) + Math.pow(x - centerX, 2));
+            const distance2 = Math.sqrt(Math.pow(y - (centerY + distance / 2), 2) + Math.pow(x - centerX, 2));
+            const phase1 = 2 * Math.PI * (distance1 * frequency / speed) - 2 * Math.PI * frequency * t * animationSpeed;
+            const phase2 = 2 * Math.PI * (distance2 * frequency / speed) - 2 * Math.PI * frequency * t * animationSpeed;
+            const intensity1 = amplitude * Math.cos(phase1);
+            const intensity2 = amplitude * Math.cos(phase2);
+            const interference = - 200 + intensity1 + intensity2 * twoSourcesFlag;
+            const color = interference > 0 ? `rgb(${interference},0,0)` : `rgb(0,0,${-interference})`;
+            if (Math.abs(distance1) >= Math.abs(propagation)){
+              g.beginFill(interference).drawRect(x, y, res, res);
+            } else {
+              g.beginFill(color).drawRect(x, y, res, res);
+            }
+            
+          }
+        }
+        const bg = new createjs.Shape(g);
+        const blurFilter = new createjs.BlurFilter(11, 11);
+        bg.filters = [blurFilter];
+        bg.cache(0, 0, waveSpace.nominalBounds.width, waveSpace.nominalBounds.height);
+        waveSpace.addChild(bg);
+        if (twoSourcesFlag){
+          container.addChild(s1, s2);
+        } else {
+          container.addChild(s1);
+        }
+        
+        requestAnimationFrame(draw);
+      }
+      console.log("distance:   " + distance);
+      console.log("wavelength: " + wavelength);
+      console.log("frequency:  " + frequency);
+      console.log("speed:      " + speed);
+      console.log("amplitude:  " + amplitude);
+      console.log("anim speed: " + animationSpeed);
+      console.log(" ");
+
+    }
+
+    playBtn.visible = true;
+    stopBtn.visible = false;
+
+    playstop.addEventListener("click", function(){
+      playFlag = !playFlag;
+      if (!playFlag){
+        t0 = t;
+      } else if (playFlag){
+        dt = createjs.Ticker.getTime() / 1000 - t0;
+      }
+      draw();
+      playBtn.visible = !playBtn.visible;
+      stopBtn.visible = !stopBtn.visible;
+    });
+
+    radBtn1s.dot.visible = false;
+    radBtn2s.dot.visible = true;
+
+    radBtn1s.on("click", function(){
+      this.dot.visible = true;
+      radBtn2s.dot.visible = !this.dot.visible;
+      twoSourcesFlag = false;
+      container.removeChild(s2);
+    });
+
+    radBtn2s.on("click", function(){
+      this.dot.visible = true;
+      radBtn1s.dot.visible = !this.dot.visible;
+      twoSourcesFlag = true;
+      container.addChild(s1, s2);
+    });
+
+    sliderAnimSpeed.on("pressmove", function(){
+      var pt = this.globalToLocal(stage.mouseX, stage.mouseY);
+      var slidingBound = (this.path.nominalBounds.width - this.handle.nominalBounds.width) / 2;
+      if (pt.x > - slidingBound && pt.x < slidingBound){
+        this.handle.x = pt.x;
+        animationSpeed = (pt.x + 75) / 160 * 0.3 + 0.02;
+      }
+    });
+
+    sliderDistance.on("pressmove", function(){
+      var pt = this.globalToLocal(stage.mouseX, stage.mouseY);
+      var slidingBound = (this.path.nominalBounds.width - this.handle.nominalBounds.width) / 2;
+      if (pt.x > - slidingBound && pt.x < slidingBound){
+        if(!playFlag){
+          waveSpace.removeAllChildren();
+        }
+        this.handle.x = pt.x;
+        distance = (pt.x + 75) / 160 * 450;
+        s1.y = centerY - distance / 2;
+        s2.y = centerY + distance / 2;
+        if (twoSourcesFlag){
+          container.addChild(s1, s2);
+        } else {
+          container.addChild(s1);
+        }
+      }
+    });
+
+    sliderFreq.on("pressmove", function(){
+      var pt = this.globalToLocal(stage.mouseX, stage.mouseY);
+      var slidingBound = (this.path.nominalBounds.width - this.handle.nominalBounds.width) / 2;
+      if (pt.x > - slidingBound && pt.x < slidingBound){
+        this.handle.x = pt.x;
+        frequency = (pt.x + 75) / 160 * 16 + 2;
+      }
+    });
+
+    sliderAmp.on("pressmove", function(){
+      var pt = this.globalToLocal(stage.mouseX, stage.mouseY);
+      var slidingBound = (this.path.nominalBounds.width - this.handle.nominalBounds.width) / 2;
+      if (pt.x > - slidingBound && pt.x < slidingBound){
+        this.handle.x = pt.x;
+        amplitude = (pt.x + 75) / 160 * 60 + 5;
+      }
+    });
+
+  }
+}
--- simulation.js
+++ simulation.js
@@ -1,9 +1,12 @@
 class Wavefront{
-  constructor(_root, _stage, _cx, _cy, _t0){
+  constructor(_root, _stage, _lib, _cx1, _cy1, _cx2, _cy2, _t0){
       this.stage = _stage;
       this.root = _root;
-      this.cx = _cx;
-      this.cy = _cy;
+      this.lib = _lib;
+      this.cx1 = _cx1;
+      this.cy1 = _cy1;
+      this.cx2 = _cx2;
+      this.cy2 = _cy2;
       this.t0 = _t0;
       this.init();
   }
@@ -11,22 +14,42 @@ class Wavefront{
   init(){
       var stage = this.stage,
         root = this.root,
+        lib = this.lib,
         containerDiagonal = Math.sqrt(Math.pow(this.root.nominalBounds.width, 2) + Math.pow(this.root.nominalBounds.height, 2)),
-        cx = this.cx,
-        cy = this.cy,
+        cx1 = this.cx1,
+        cy1 = this.cy1,
+        cx2 = this.cx2,
+        cy2 = this.cy2,
         t0 = this.t0,
         radius = 0,
         t = 0,
+        distance = 225,
         propagation = 0,
         wavelength = 0,
         speed = 440,
         frequency = 8,
         amplitude = 30,
-        animationSpeed = 0.15;
+        animationSpeed = 0.15,
+        twoSourcesFlag = true;
 
-      var frontDiff = new createjs.Shape();
-      frontDiff.cx = cx;
-      frontDiff.cy = cy;
+        const centerX = 0;
+        const centerY = root.nominalBounds.height / 2;
+/*
+        var s1 = new lib.Source();
+        var s2 = new lib.Source();
+        s1.x = s2.x = centerX + 15;
+        s1.y = centerY - distance / 2;
+        s2.y = centerY + distance / 2;
+        if (twoSourcesFlag){
+          root.addChild(s1, s2);
+        } else {
+          root.addChild(s1);
+        }
+*/
+      var frontDiff1 = new createjs.Shape();
+      var frontDiff2 = new createjs.Shape();
+      //frontDiff.cx = cx;
+      //frontDiff.cy = cy;
 
       stage.addEventListener("tick", enlarge);
       function enlarge(){
@@ -36,25 +59,43 @@ class Wavefront{
         wavelength = speed / frequency;
         var innerFront = propagation - wavelength /2,
             outerFront = propagation + wavelength /2;
-        frontDiff.graphics.clear();
+        frontDiff1.graphics.clear();
+        frontDiff2.graphics.clear();
         for (let radius = innerFront; radius <= outerFront; radius += 2){
-          const phase1 = 2 * Math.PI * (radius * frequency / speed) - 2 * Math.PI * frequency * t * animationSpeed;
-          const phase2 = 2 * Math.PI * (radius * frequency / speed) - 2 * Math.PI * frequency * t * animationSpeed;
-          const intensity1 = - 200 + amplitude * Math.cos(phase1);
-          const intensity2 = - 200 + amplitude * Math.cos(phase2);
-          const color = intensity > 0 ? `rgb(${intensity},0,0)` : `rgb(0,0,${-intensity})`;
-          frontDiff.graphics.setStrokeStyle(2).beginStroke(color);
-          if (radius >= 0){
-            frontDiff.graphics.drawCircle(cx, cy, radius);
+          for (let angle = -180; angle <= 180; angle++){
+            const angle_rad1 = angle / 180 * Math.PI;
+            const angle_rad2 = (angle + 1) / 180 * Math.PI;
+            const distance1 = Math.sqrt(Math.pow(radius, 2) + Math.pow(radius, 2));
+            const distance2 = Math.sqrt(Math.pow(radius, 2) + Math.pow(radius, 2));
+            const phase1 = 2 * Math.PI * (radius * frequency / speed) - 2 * Math.PI * frequency * t * animationSpeed;
+            const phase2 = 2 * Math.PI * (radius * frequency / speed) - 2 * Math.PI * frequency * t * animationSpeed;
+            const intensity1 = amplitude * Math.cos(phase1);
+            const intensity2 = amplitude * Math.cos(phase2);
+            const interference = - 200 + intensity1 + intensity2 * twoSourcesFlag;
+            const color = interference > 0 ? `rgb(${interference},0,0)` : `rgb(0,0,${-interference})`;
+            const color1 = intensity1 > 0 ? `rgb(${intensity1},0,0)` : `rgb(0,0,${-intensity1})`;
+            const color2 = intensity2 > 0 ? `rgb(${intensity2},0,0)` : `rgb(0,0,${-intensity2})`;
+            frontDiff1.graphics.setStrokeStyle(2).beginStroke(color);
+            frontDiff2.graphics.setStrokeStyle(2).beginStroke(color);
+            if (radius >= 0){
+              frontDiff1.graphics.arc(cx1, cy1, radius, angle_rad1, angle_rad2 , false);
+              frontDiff2.graphics.arc(cx2, cy2, radius, angle_rad1, angle_rad2 , false);
+              //stage.addChild(Arc[i]);
+              //frontDiff.graphics.drawCircle(cx1, cy1, radius);
+              //frontDiff.graphics.drawCircle(cx2, cy2, radius);
+            }
+            root.addChild(frontDiff1);
+            root.addChild(frontDiff2);
           }
-          root.addChild(frontDiff);
+          
         }
         
         
         
         //root.cache(0, 0, root.nominalBounds.width, root.nominalBounds.height);
         if (propagation > containerDiagonal){
-          root.removeChild(frontDiff);
+          root.removeChild(frontDiff1);
+          root.removeChild(frontDiff2);
           stage.removeEventListener("tick", enlarge);
         }
         //console.log("distance:   " + distance);
@@ -72,9 +113,10 @@ class Wavefront{
 }
 
 class Simulation {
-  constructor(_root, _stage){
+  constructor(_root, _stage, _lib){
     this.root = _root;
     this.stage = _stage;
+    this.lib = _lib;
     this.container = this.root.container;
     this.waveSpace = this.container.waveSpace;
     this.header = this.root.header;
@@ -88,6 +130,7 @@ class Simulation {
   init(){
     var stage = this.stage;
     var waveSpace = this.waveSpace;
+    var lib = this.lib;
     var playstop = this.playstop;
     var playBtn = this.playBtn;
     var stopBtn = this.stopBtn;
@@ -114,8 +157,8 @@ class Simulation {
       t0 = createjs.Ticker.getTime() / 1000;
       playFlag = !playFlag;
       if (playFlag){
-        var wave = new Wavefront(waveSpace, stage, x1, y1, t0);
-        var wave = new Wavefront(waveSpace, stage, x2, y2, t0);
+        var wave = new Wavefront(waveSpace, stage, lib, x1, y1, x2, y2, t0);
+        //var wave = new Wavefront(waveSpace, stage, x2, y2, t0);
       }
       
       playBtn.visible = !playBtn.visible;
